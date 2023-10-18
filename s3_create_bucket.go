8a80a3dc4a6922985dbf1a152d57a616a20f62c8
ranjankuldeep <ranjankuldeep19@gmail.com>
2023-10-18 12:36:15 +0530

    bug: added bucket parameter

--- s3_create_bucket.go
+++ s3_create_bucket.go
@@ -2,19 +2,13 @@ package awsbucket
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func CreateBucket() {
-	if len(os.Args) != 2 {
-		exitError("Bucket name missing!\nUsage: %s bucket_name", os.Args[0])
-	}
-
-	bucket := os.Args[1]
+func CreateBucket(bucket string) {
 
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-west-2")},
